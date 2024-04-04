696263a36a858d5df6daa9dc846b9323b72a23d3
HyGigs <giggs.samestagiga+github@gmail.com>
2024-04-04 12:10:28 +0700

    add login

--- app/src/main/java/com/example/akangfood/MainActivity.java
+++ app/src/main/java/com/example/akangfood/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.akangfood.Fragments.FavoriteFragment;
 import com.example.akangfood.Fragments.MakananFragment;
@@ -22,42 +23,47 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private TextView tvNameUser, tvEmailUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding =  ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = binding.drawerLayout;
+        navigationView = binding.navView;
+        toolbar = binding.toolbar;
 
         setSupportActionBar(toolbar);
 
+        // Get reference to TextViews in header layout
+        tvNameUser = navigationView.getHeaderView(0).findViewById(R.id.tvNameUser);
+        tvEmailUser = navigationView.getHeaderView(0).findViewById(R.id.tvEmailUser);
+
+        // Retrieve name and email from intent extras
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("nameUser");
+        String email = intent.getStringExtra("emailUser");
+
+        // Update TextViews in header layout
+        tvNameUser.setText(name);
+        tvEmailUser.setText(email);
+
+        // Setup navigation drawer
         navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
+        // Initialize with default fragment
         replaceFragment(new MakananFragment());
         navigationView.setCheckedItem(R.id.menu_makanan);
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else{
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -82,7 +88,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
-
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
